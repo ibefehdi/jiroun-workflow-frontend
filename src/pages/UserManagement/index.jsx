8c5055c7a9d71a813d3eddd5620e9e7c4a8f3e60
fahad <fahadasad.aou@gmail.com>
2023-07-26 16:07:34 +0300

    Chore: added: Users management, need to work on the pagination will do tomorrow

--- src/pages/UserManagement/index.jsx
+++ src/pages/UserManagement/index.jsx
@@ -1,20 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { Container } from 'reactstrap'
+import axiosInstance from '../../constants/axiosConstant'
+import TableContainer from '../../components/TableContainer'
+import { useGETAPI } from '../../hooks/useGETAPI'
 
 const UserManagement = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false)
+    const { data, fetchData, pageCount, totalDataCount, loadStatus } = useGETAPI(
+        axiosInstance.get,
+        '/users',
+        'status',
+        'data'
+    );
+
+    useEffect(() => {
+        fetchData({
+            pageSize: 10,
+            pageIndex: 1,
+        });
+    }, [fetchData])
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'First Name',
+                accessor: 'fName',
+            },
+            {
+                Header: 'Last Name',
+                accessor: 'lName',
+            },
+            {
+                Header: 'Username',
+                accessor: 'username',
+            },   
+            {
+                Header: 'Occupation',
+                accessor: 'occupation',
+            },
+        ],
+        []
+    );
+    if (loadStatus) {
+        return <div>Loading...</div>;
+    }
     return (
         <Container className={sidebarOpen ? 'pagecontainer' : 'pagecontainer-closed'}>
             <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-            <h1>User Management</h1>
-
+            <TableContainer data={data} columns={columns} />
         </Container>
     )
 }
