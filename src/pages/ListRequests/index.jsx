f050c2ce428720d4d68a2e88d7ae69227c396707
fahadsmartlifekw <132986028+fahadsmartlifekw@users.noreply.github.com>
2023-09-13 15:19:16 +0300

    List all request modal

--- src/pages/ListRequests/index.jsx
+++ src/pages/ListRequests/index.jsx
@@ -1,65 +1,112 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import { Button, Container, Modal, ModalBody, ModalHeader, Progress, Table } from 'reactstrap';
+import { Button, Container, Modal, ModalBody, ModalFooter, ModalHeader, Progress, Table } from 'reactstrap';
 import axiosInstance from '../../constants/axiosConstant';
 import { useGETAPI } from '../../hooks/useGETAPI';
 import TableContainer from '../../components/TableContainer';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-const RequestDetail = ({ requestType, project, items }) => {
-    const { projectName, projectManager, projectDirector } = project;
+const RequestDetail = ({ requestDetail }) => {
+    const wasFinalized = (isFinalized) => {
+        if (isFinalized === 1) {
+            return "Approved"
+        } else if (isFinalized === 2) {
+            return "Was sent back for more information"
+        }
+        else if (isFinalized === 0) {
+            return "Was Declined"
+        }
+    }
+    const renderRequestPayment = () => {
+        if (requestDetail?.requestType === "Request Payment") {
+            return (
+                <>
+                    <tr>
+                        <td><strong>Payment Type:</strong></td>
+                        <td>{requestDetail?.paymentType}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Contractor for payment:</strong></td>
+                        <td>{requestDetail?.contractorForPayment?.fName} {requestDetail?.contractorForPayment?.lName}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Estimated Amount:</strong></td>
+                        <td>{requestDetail?.estimatedAmount}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Required Amount:</strong></td>
+                        <td>{requestDetail?.requiredAmount}</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><strong>Paid Amount:</strong></td>
+                        <td>{requestDetail?.paidAmount}</td>
+                        <td></td>
+                    </tr>
+
+
+                </>
+            );
+        }
+        return null;
+    };
+
     return (
-        <>
-            <tr>
-                <th>Request Type</th>
-                <td>{requestType}</td>
-            </tr>
-            <tr>
-                <th>Project Name</th>
-                <td>{projectName}</td>
-            </tr>
-            <tr>
-                <th>Project Managers</th>
-                <td>
-                    {projectManager?.map(({ fName, lName }, index) => (
-                        <div key={index}>
-                            {fName} {lName}
-                        </div>
-                    ))}
-                </td>
-            </tr>
-            <tr>
-                <th>Project Director</th>
-                <td>{projectDirector.fName} {projectDirector.lName}</td>
-            </tr>
-            {items && items.map((item, index) => (
-                <React.Fragment key={index}>
-                    <tr>
-                        <th>Item {index + 1} Name</th>
+        <Table className="details-table" hover bordered striped>
+            <tbody>
+                <tr><td><strong>Request ID:</strong></td><td>{requestDetail?.requestID}</td><td></td></tr>
+                <tr><td><strong>Request Type:</strong></td><td>{requestDetail?.requestType}</td><td></td></tr>
+                <tr><td><strong>Project Name:</strong></td><td>{requestDetail?.project?.projectName}</td><td></td></tr>
+                <tr><td><strong>Project Year:</strong></td><td>{new Date(requestDetail?.project?.year).getFullYear()}</td><td></td></tr>
+                <tr><td><strong>Project Location:</strong></td><td>{requestDetail?.project?.location}</td><td></td></tr>
+                {requestDetail?.requestType === "Request Item" && (<tr><td style={{ fontWeight: "bolder" }}>Item Name</td><td style={{ fontWeight: "bolder" }}>Quantity</td><td style={{ fontWeight: "bolder" }}>BOQ ID</td></tr>)}
+
+                {requestDetail?.requestType === "Request Item" && requestDetail?.items?.map((item, index) => (
+                    <tr key={index}>
                         <td>{item.itemName}</td>
-                    </tr>
-                    <tr>
-                        <th>Item {index + 1} Quantity</th>
                         <td>{item.itemQuantity}</td>
+                        <td>{item.boqId}</td>
+
                     </tr>
-                </React.Fragment>
-            ))}
-        </>
+                ))}
+                {(renderRequestPayment())}
+                {requestDetail?.subRequests?.map((subRequest, index) => (
+                    <tr key={index}>
+                        <td><strong>Sub Request {index + 1}:</strong></td>
+                        <td>
+                            <div><strong>Sender:</strong> {`${subRequest?.sender?.fName} ${subRequest?.sender?.lName}`}</div>
+                            <div><strong>Recipient:</strong> {`${subRequest?.recipient?.fName} ${subRequest?.recipient?.lName}`}</div>
+                            <div><strong>Was the subrequest approved?</strong> {wasFinalized(subRequest?.isFinalized)}</div>
+                            <div><strong>Comments:</strong> {subRequest?.comments}</div>
+                            <div>
+                                <strong>Sent at: </strong>{new Date(subRequest.subRequestSentAt).toLocaleString()}
+                            </div>
+
+                        </td>
+                        <td></td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
     );
 };
 
 const RequestDetailModal = ({ isOpen, toggle, requestDetail }) => {
     if (!requestDetail) return null;
     return (
-        <Modal isOpen={isOpen} toggle={toggle}>
+        <Modal isOpen={isOpen} toggle={toggle} style={{maxWidth:"70rem"}}>
             <ModalHeader toggle={toggle}>Request Detail</ModalHeader>
             <ModalBody>
                 <Table>
                     <tbody>
-                        <RequestDetail {...requestDetail} />
+                        <RequestDetail requestDetail={requestDetail} />
                     </tbody>
                 </Table>
             </ModalBody>
+            
         </Modal>
     );
 };
@@ -125,7 +172,7 @@ const ListRequests = () => {
                 Cell: ({ value, row }) => {  // Note the use of row here
                     const requestType = row.original.requestType; // Access requestType from row
                     return (
-                        <div style={{ width: "80px" ,height:"80px"}}>
+                        <div style={{ width: "80px", height: "80px" }}>
                             <CircularProgressbar value={value} text={`${value}%`} strokeWidth={5}
                             />
 
