5a6b36925fe4ffe32ae420f77c517f23df44661e
fahadsmartlifekw <132986028+fahadsmartlifekw@users.noreply.github.com>
2024-01-25 14:29:53 +0300

    Bug fix: Rich Text

--- src/pages/ApprovedItemRequests/index.jsx
+++ src/pages/ApprovedItemRequests/index.jsx
@@ -160,7 +160,8 @@ const ApprovedItemRequests = () => {
                                         <div><strong>Sender:</strong> {`${subRequest?.sender?.fName} ${subRequest?.sender?.lName}`}</div>
                                         <div><strong>Recipient:</strong> {`${subRequest?.recipient?.fName} ${subRequest?.recipient?.lName}`}</div>
                                         <div><strong>Was the subrequest approved?</strong> {wasFinalized(subRequest?.isFinalized)}</div>
-                                        <div><strong>Comments:</strong> {subRequest?.comments}</div>
+                                        <div><strong>Comments:</strong><div dangerouslySetInnerHTML={{ __html: subRequest?.comments }} />
+                                        </div>
                                         <div>
                                             <strong>Sent at: </strong>{new Date(subRequest.subRequestSentAt).toLocaleString()}
                                         </div>
